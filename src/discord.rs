345d773944a5ca0d644d4dd72d28268474a9b409
4jamesccraven <4jamesccraven@gmail.com>
2025-03-11 11:34:42 -0400

    Implement baro command

--- src/discord.rs
+++ src/discord.rs
@@ -1,3 +1,46 @@
+use crate::api::{VoidTrader, NewsItem};
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+// use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+pub struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        let response: Option<String> = match msg.content.as_str() {
+            "!baro" => {
+                if let Ok(trader) = VoidTrader::get().await {
+                    let content = trader.message();
+
+                    Some(content)
+                } else {
+                    let msg = "Unable to get information on the void trader. \
+                    Please try again";
+
+                    Some(msg.into())
+                }
+            },
+            response if response.starts_with("!") => {
+                let msg = "Unknown command";
+
+                Some(msg.into())
+            },
+            _ => {
+                None
+            },
+        };
+
+        if let Some(response) = response {
+            if let Err(why) = msg.channel_id.say(&ctx.http, response).await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
 pub trait ToDiscordMessage {
     fn message(&self) -> String;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,37 @@ mod api;
 mod date;
 mod discord;
 
-use api::{NewsItem, VoidTrader};
-use discord::ToDiscordMessage;
+use discord::Handler;
 
 use std::boxed::Box;
+use std::env;
 use std::error::Error;
 
+use dotenv::dotenv;
+use serenity::prelude::*;
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let trader = VoidTrader::get().await?;
+    let _ = dotenv()?;
 
-    println!("{}", trader.message());
+    let token_err = "Unable to get discord application token. \
+                     Please ensure that the environment variable \
+                     `TOKEN` is available or present in /.env";
+
+    let token = env::var("TOKEN")
+        .expect(token_err);
+
+    let intents = GatewayIntents::default()
+        | GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler).await?;
+
+    if let Err(why) = client.start().await {
+        eprintln!("Client error: {why:?}");
+    }
 
     Ok(())
 }
